2311644db689896d10bf11aebb8397ee9c30c55a
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-09-22 13:04:20 +0530

    fixing error in problem statement

--- callback3.js
+++ callback3.js
@@ -1,4 +1,4 @@
-//Problem 1:  Write a function that will return all cards that belong to a particular list based on the listID that is passed to it from the given data in cards.json. Then pass control back to the code that called it by using a callback function.
+//Problem 3:  Write a function that will return all cards that belong to a particular list based on the listID that is passed to it from the given data in cards.json. Then pass control back to the code that called it by using a callback function.
 
 function callback3(boardId, cards, callback) {
   setTimeout(() => {
